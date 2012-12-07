b788c8ecaa935035784eff3eaa5590b83c36092e
GaussCheng <GaussCheng@gmail.com>
2012-12-07 19:43:18 +0800

    1.change the definination of const variable cause rake 0.9.2 is not supprot

--- lib/project_builders/mplabx_project_builder.rb
+++ lib/project_builders/mplabx_project_builder.rb
@@ -3,6 +3,7 @@ require 'rake'            # for ext() method
 require 'securerandom'
 require 'rexml/document'
 require 'project_builder'
+require 'find'
 
 include REXML
 
@@ -31,11 +32,18 @@ class MplabxProjectBuilder < ProjectBuilder
 
   def initialize(coding = "GBK")
     super(coding)
-    @uuid = SecureRandom.uuid 
+    @uuid = uuid 
     @configs = Document.new
     @project = Document.new
     @project << XMLDecl.new("1.0", "UTF-8")
     @configs << XMLDecl.new("1.0", "UTF-8")
+  end
+  
+  def self.uuid
+    ary = SecureRandom.random_bytes(16).unpack("NnnnnN")
+    ary[2] = (ary[2] & 0x0fff) | 0x4000
+    ary[3] = (ary[3] & 0x3fff) | 0x8000
+    "%08x-%04x-%04x-%04x-%04x%08x" % ary
   end
   
   def generate
@@ -52,9 +60,11 @@ class MplabxProjectBuilder < ProjectBuilder
     @project.write(out_file, 2)
     out_file.close()
     
-    Object.const_get("COLLECTION_PATHS_SOURCE").each do |path|
-      puts path
-    end
+    # Object.const_get("COLLECTION_PATHS_SOURCE").each do |path|
+      # puts path
+    # end
+    
+    file_walk(".", ["./vendor/ceedling"], [".c", ".h"] )
   end
   
   def generate_configurations
@@ -84,5 +94,23 @@ class MplabxProjectBuilder < ProjectBuilder
     end
   end
   
+ def file_walk(path, exclude_paths, include_ext)
+    Find.find(path) do |f|
+      if FileTest.directory?(f)
+        if exclude_paths.include?(f)
+          Find.prune
+        else
+          next
+        end
+      end
+      if include_ext.include?(get_file_ext(f))
+        puts f
+      end
+    end
+  end
+  
+  def get_file_ext(file)
+    file[/\.[^\.]+$/]
+  end
   
 end
